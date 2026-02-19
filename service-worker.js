83a0d4a055f8bcb78f735666cc0bacb909b1b8b8
pmaurizf-lgtm <pmaurizf@gmail.com>
2026-02-19 18:49:51 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jornada-pro-v1.0.0";
+const CACHE_NAME = "jornada-pro-v1.1.0";
 
 const ASSETS = [
   "./",
@@ -6,6 +6,20 @@ const ASSETS = [
   "./styles.css",
   "./app.js",
   "./manifest.json",
+
+  // CORE
+  "./core/storage.js",
+  "./core/state.js",
+  "./core/calculations.js",
+  "./core/bank.js",
+  "./core/holidays.js",
+  "./core/notifications.js",
+
+  // UI
+  "./ui/theme.js",
+  "./ui/charts.js",
+
+  // ICONOS
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
